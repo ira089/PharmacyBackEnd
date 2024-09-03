f38f9541a07007afe9cc2644b82cc9803c81dd65
ira089 <gvir2015@gmail.com>
2024-09-03 23:40:52 +0300

    randomSixPharmacies

--- routes/storesRouter.js
+++ routes/storesRouter.js
@@ -4,5 +4,6 @@ import storeControllers from "../controllers/storeControllers.js";
 const storesRouter = express.Router();
 
 storesRouter.get("/", storeControllers.store);
+storesRouter.get("/six", storeControllers.storeSix);
 
 export default storesRouter;
