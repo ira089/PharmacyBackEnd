94661193d44c25adfeb736c3991795e927438813
ira089 <gvir2015@gmail.com>
2024-09-12 12:26:41 +0300

    Update orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,12 +3,13 @@ import orderControllers from "../controllers/orderControllers.js";
 import authenticate from "../middlewares/authenticate.js";
 import { orderSchema} from "../schemas/ordersShema.js";
 import validateBody from "../decorators/validateBody.js";
+import {isValidId} from "../middlewares/isValidId.js";
 
 const orderRouter = express.Router();
 orderRouter.use(authenticate);
 
 // orderRouter.get("/", cartControllers.cartsAll);
-orderRouter.put("/update", validateBody(orderSchema), orderControllers.updOrder);
+orderRouter.put("/:id", isValidId, validateBody(orderSchema), orderControllers.updOrder);
 orderRouter.post("/", validateBody(orderSchema), orderControllers.addOrder);
 
 
