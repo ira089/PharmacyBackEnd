39517ebf7e8fb3514c9dff0d55add4f0c5ea6ddb
ira089 <gvir2015@gmail.com>
2024-09-12 18:03:56 +0300

    updServes

--- routes/orderItemsRouter.js
+++ routes/orderItemsRouter.js
@@ -1,17 +1,25 @@
 import express from "express";
 import orderItemsControllers from "../controllers/orderItemsControlles.js";
 import authenticate from "../middlewares/authenticate.js";
-import { orderItemsSchema} from "../schemas/ordersShema.js";
+import { orderItemsSchema } from "../schemas/ordersShema.js";
 import validateBody from "../decorators/validateBody.js";
-import {isValidId} from "../middlewares/isValidId.js";
+import { isValidId } from "../middlewares/isValidId.js";
 
 const orderItemsRouter = express.Router();
 orderItemsRouter.use(authenticate);
 
 orderItemsRouter.get("/", orderItemsControllers.allOrderItem);
-orderItemsRouter.put("/:id", isValidId, validateBody(orderItemsSchema), orderItemsControllers.updOrderItem);
-orderItemsRouter.post("/", validateBody(orderItemsSchema), orderItemsControllers.addOrderItem);
-orderItemsRouter.delete("/:id", isValidId, validateBody(orderItemsSchema), orderItemsControllers.delOrderItem);
-
+orderItemsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(orderItemsSchema),
+  orderItemsControllers.updOrderItem
+);
+orderItemsRouter.post(
+  "/",
+  validateBody(orderItemsSchema),
+  orderItemsControllers.addOrderItem
+);
+orderItemsRouter.delete("/:id", isValidId, orderItemsControllers.delOrderItem);
 
 export default orderItemsRouter;
--- services/orderItemsService.js
+++ services/orderItemsService.js
@@ -1,10 +1,21 @@
 import Orderitem from "../models/Orderitem.js";
 
-export const allOrderItemsService = (filter = {}) => Orderitem.find(filter, "-createdAt -updatedAt").populate("owner", "total totalQuantity");
+export const allOrderItemsService = (filter = {}) =>
+  Orderitem.find(filter, "-createdAt -updatedAt").populate(
+    "owner",
+    "total totalQuantity"
+  );
 
-export const addOrderItemService = data => Orderitem.create(data);
+// export const addOrderItemService = (data) => Orderitem.create(data);
+export const addOrderItemService = async (data) => {
+  const newItem = await Orderitem.create(data);
+  return Orderitem.findById(newItem._id).select("_id idProduct quantity");
+};
 
 export const updOrderItemService = (filter, data) =>
-    Orderitem.findOneAndUpdate(filter, data);
+  Orderitem.findOneAndUpdate(filter, data, { new: true }).select(
+    "_id idProduct quantity"
+  );
 
-export const deleteOrderItemService = filter => Orderitem.findOneAndDelete(filter);
+export const deleteOrderItemService = (filter) =>
+  Orderitem.findOneAndDelete(filter).select("_id idProduct quantity");
