07a745c346835f69bddecfe8d9ed66ebbfcb1cdd
ira089 <gvir2015@gmail.com>
2024-09-12 18:45:20 +0300

    Update ordersShema.js

--- schemas/ordersShema.js
+++ schemas/ordersShema.js
@@ -1,32 +1,19 @@
 import Joi from "joi";
 import * as userConstants from "../constants/userConstants.js";
 
-
 export const orderSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
   address: Joi.string(),
-  paymentMethod: Joi.string()
-    .valid(...userConstants.paymentMethodList),
-    total: Joi.string().pattern(userConstants.priceRegepxp).required(),
-    totalQuantity: Joi.string().pattern(userConstants.quantityRegepxp).required(),
-    status: Joi.string().valid(...userConstants.orderStatusList),
+  paymentMethod: Joi.string().valid(...userConstants.paymentMethodList),
+  total: Joi.string().pattern(userConstants.priceRegepxp).required(),
+  totalQuantity: Joi.string().pattern(userConstants.quantityRegepxp).required(),
+  status: Joi.string().valid(...userConstants.orderStatusList),
 });
 
 export const orderItemsSchema = Joi.object({
   idProduct: Joi.string().required(),
   idOrder: Joi.string().required(),
   quantity: Joi.string().pattern(userConstants.quantityRegepxp).required(),
-  })
-
-// export const orderUpdSchema = Joi.object({
-//   name: Joi.string(),
-//   email: Joi.string(),
-//   phone: Joi.string(),
-//   address: Joi.string(),
-//   paymentMethod: Joi.string().valid(...userConstants.paymentMethodList),
-//   total: Joi.string().pattern(userConstants.priceRegepxp).required(),
-//   totalQuantity: Joi.string().pattern(userConstants.quantityRegepxp).required(),
- 
-// });
+});
