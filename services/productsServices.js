a4ce8e4229af1c2a5167c41c7814a69863a3a7a5
ira089 <gvir2015@gmail.com>
2024-09-15 12:59:21 +0300

    Update productsServices.js

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,7 +1,7 @@
 import Product from "../models/Product.js";
 
 export const getAllproducts = (filter = {}, setting = {}) => {
-  return Product.find(filter, setting);
+  return Product.find(filter, "photo name price suppliers", setting);
 };
 
 export const countProducts = (filter) => Product.countDocuments(filter);
